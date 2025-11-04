e2e4ee20d31faf331a390d5e8ee3d27857a05947
IspahanRahman <ispahanrahman133@gmail.com>
2025-11-04 11:47:55 +0600

    feat: Implement public authentication layout and login page
    - Added PublicPanelLayout component for the public authentication layout.
    - Created LoginPage component that renders the Login form.
    - Established a global layout with locale support and theme provider.
    - Introduced NotFound component for handling 404 errors.
    - Set up HomePage to redirect users to the dashboard based on locale.
    - Developed UserNavbar component for navigation with theme toggle functionality.
    - Created CustomIconButton component for reusable icon buttons with theming.
    - Configured internationalization with routing and request handling.
    - Implemented Login component with form handling for user authentication.

---
+++ src/app/[locale]/(public)/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState } from "react";
+import UserNavbar from "@/components/layouts/PublicNavbar";
+
+import { ReactNode } from "react";
+
+export default function PublicPanelLayout({ children }: { children: ReactNode }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="min-h-dvh grid grid-rows-[auto_1fr] bg-surface text-foreground">
+      <UserNavbar onMenuClick={() => setSidebarOpen(true)} />
+
+      <main className="h-full px-4">
+        <div className="h-full grid place-items-center">{children}</div>
+      </main>
+    </div>
+  );
+}
---
+++ src/app/[locale]/(public)/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import Login from "@/pages/auth/Login";
+
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
